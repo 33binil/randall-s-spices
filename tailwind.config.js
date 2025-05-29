376b929e436e007be58701c3bfd636e74bf2b8fd
Binil B <binilb@Binils-MacBook-Air.local>
2025-05-29 13:59:17 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkbg: '#0F1F14',
+        primary: '#1A3C34',
+        herbal: '#5FC58C',
+        turmeric: '#D9A441',
+        softwhite: '#F1F1F1',
+        muted: '#A8B2A1',
+        limehover: '#8EFFB7'
+      }
+    }
+
+  },
+  plugins: [],
+}
